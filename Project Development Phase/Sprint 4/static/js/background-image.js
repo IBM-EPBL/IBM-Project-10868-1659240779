c5dddddf5106db37ff8cd4415c042ee3bd4590da
Sharatn7 <sarathn08@gmail.com>
2022-11-18 14:10:47 +0530

    minor tweeks

--- Project Development Phase/Sprint 4/static/js/background-image.js
+++ Project Development Phase/Sprint 4/static/js/background-image.js
@@ -1,6 +1,6 @@
 (async () => {
   const res = await fetch("https://foodish-api.herokuapp.com/api/");
-  const body = document.querySelector("body");
+  const body = document.querySelector("home");
 
   const resJSON = await res.json();
   body.style.backgroundImage = `url(${resJSON.image})`;
